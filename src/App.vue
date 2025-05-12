b630b4b42c2473f23c4eb152a4e94923ff130ca6
Daniil Maykovskiy <daniil.maikovsky@yandex.ru>
2025-05-12 19:48:57 +0300

    upd

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,14 @@
 <script>
-import TheNavbar from "./the-navbar.vue";
-
 export default {
-  components: {
-    TheNavbar,
-  },
+  components: {},
   data() {
-    return {
-      isAuth: true,
-    };
-  },
-  methods: {
-    login() {
-      this.isAuth = true;
-      this.$router.push(this.$route.query.from ?? "/dashboard");
-    },
-    logout() {
-      this.isAuth = false;
-      this.$router.push({
-        path: "/login",
-        query: {
-          from: this.$route.path,
-        },
-      });
-    },
-  },
-  provide() {
-    return {
-      login: this.login,
-      logout: this.logout,
-      emails: [
-        { id: "1", theme: "Купил себе PlayStation 5" },
-        { id: "2", theme: "Выучил Vue Router" },
-        { id: "3", theme: "Хочу изучить весь Vue" },
-        { id: "4", theme: "А следующий блок про Vuex!" },
-        { id: "5", theme: "А что там насчет Vue Hooks?" },
-      ],
-    };
+    return {};
   },
 };
 </script>
 
 <template>
-  <the-navbar :visible="isAuth"></the-navbar>
-  <div class="container">
-    <router-view></router-view>
-  </div>
+  <div class="container"></div>
 </template>
 
 <style>
