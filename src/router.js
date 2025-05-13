c30de4f9025a07f4032f060173b8a18428852faa
Daniil Maykovskiy <daniil.maikovsky@yandex.ru>
2025-05-13 07:01:51 +0300

    upd

--- src/router.js
+++ src/router.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomePage from "./HomePage.vue";
+import HomePage from "./views/HomePage.vue";
 
 const router = createRouter({
   routes: [
